54800d9eafe8e7b2cbe897241a6be6721aa0530a
mangilal2001warrgyizmorsch-rgb <mangilal2001.warrgyizmorsch@gmail.com>
2026-08-24 16:23:35 +0530

    chore: remove unwanted test scripts

--- inspect_kapso_media.js
+++
@@ -1,4 +0,0 @@
-import { WhatsAppClient } from '@kapso/whatsapp-cloud-api';
-const client = new WhatsAppClient({ baseUrl: 'https://api.kapso.ai/meta/whatsapp', kapsoApiKey: 'dummy' });
-console.log(Object.keys(client.media || {}));
-console.log(client.media.upload?.toString());
